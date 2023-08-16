2064259c5fa26d61610c5ebb717f00d013f03a80
mobashirul-alam <mobashirulalamseam@gmail.com>
2023-08-16 21:54:39 +0600

    tailwind setupped, favicon and title updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ function App() {
   return (
     <>
       <div>
-        <p>Welcome to the Boi Ghor</p>
+        <p className="text-center font-bold text-5xl mt-5">
+          Is Tailwind Set upped?
+        </p>
+        <p className="text-center font-semibold text-4xl mt-5 text-green-500">
+          Yes, It is !!!
+        </p>
+        <img
+          className="mx-auto w-2/4 mt-5"
+          src="/public/boi-ghor-logo-tr.png"
+          alt="logo"
+        />
       </div>
     </>
   );
